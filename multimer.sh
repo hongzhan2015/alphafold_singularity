5a8446c567ec5a3a164650f4e015e0103597c759
Jean-Yves Sgro <jsgro@wisc.edu>
2023-06-13 16:00:35 -0500

    fix databases again

--- multimer.sh
+++ multimer.sh
@@ -12,8 +12,8 @@ python3 /opt/alphafold/run_alphafold.py \
  --mgnify_database_path "$1"/mgnify/mgy_clusters_2022_05.fa  \
  --template_mmcif_dir "$1"/pdb_mmcif/mmcif_files \
  --obsolete_pdbs_path "$1"/pdb_mmcif/obsolete.dat \
- --bfd_database_path "$1"/bfd/ \
- --uniref30_database_path "$1"/uniref30 \
+ --bfd_database_path "$1"/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+ --uniref30_database_path "$1"/uniref30/UniRef30_2021_03 \
  --pdb_seqres_database_path "$1"/pdb_seqres/pdb_seqres.txt \
  --uniprot_database_path "$1"/uniprot/uniprot.fasta \
  --use_gpu_relax
